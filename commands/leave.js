8863c49a6222b2892d74cdc0fd9ba368affb9b5d
Ruan Michel Adabo <ruanbento@gmail.com>
2021-01-07 17:55:28 -0300

    [FIX] Console error while leaving (undefined return)

--- commands/leave.js
+++ commands/leave.js
@@ -1,16 +1,11 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
   if (message.member.voice.channel) {
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) {
       message.channel.send("You must be in a channel first!");
       return;
     }
-    voiceChannel
-      .leave()
-      .then(() => {})
-      .catch((e) => {
-        message.reply("I had a problem while leaving!");
-      });
+    await voiceChannel.leave();
   } else {
     message.reply("I must be in a voice channel to be banished!");
   }
