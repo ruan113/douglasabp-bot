e585e31f46c1a517ad443bebd7a667b66b4b2a5b
Ruan Michel Adabo <ruanbento@gmail.com>
2021-01-07 17:47:22 -0300

    [CREATE] Join command

---
+++ commands/join.js
@@ -0,0 +1,31 @@
+exports.run = async (client, message, args) => {
+  if (message) {
+    if(message.member.voice.channel) {
+      const voiceChannel = message.member.voice.channel;
+      if (!voiceChannel) {
+        message.channel.send("You must be in a channel first!");
+        return;
+      }
+      const connection = await voiceChannel.join();
+      if (!connection) {
+        message.channel.send("I had a problem while trying to join in!");
+        return;
+      }
+    } else {
+      message.reply("You must be in a voice channel to be banished!");
+    }
+  } else {
+    const channel = client.channels.cache.get(args.channelID);
+    if (!channel) return console.error("The channel does not exist!");
+    channel
+      .join()
+      .then((connection) => {
+        // Yay, it worked!
+        console.log("Successfully connected.");
+      })
+      .catch((e) => {
+        // Oh no, it errored! Let's log it to console :)
+        console.error(e);
+      });
+  }
+};
