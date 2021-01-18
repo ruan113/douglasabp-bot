06f68b82dc2988bc0a03e8041918997bf25e06d5
Ruan Michel Adabo <ruanbento@gmail.com>
2021-01-18 20:38:27 -0300

    [UPDATE] New gifs on refuse command

--- main.js
+++ main.js
@@ -31,11 +31,30 @@ client.on("message", (message) => {
 
   const refuseVal = Math.floor(Math.random() * Math.floor(100));
   if (refuseVal < 5) {
-    message.channel.send(
-      refuseVal % 2 === 0
-        ? "https://thumbs.gfycat.com/CarelessThankfulFluke-size_restricted.gif"
-        : "https://media2.giphy.com/media/Ju7l5y9osyymQ/giphy.gif"
-    );
+    let gif;
+    switch (Math.floor(Math.random() * Math.floor(6))) {
+      case 0:
+        gif =
+          "https://thumbs.gfycat.com/CarelessThankfulFluke-size_restricted.gif";
+        break;
+      case 1:
+        gif = "https://media2.giphy.com/media/Ju7l5y9osyymQ/giphy.gif";
+        break;
+      case 2:
+        gif = "https://gfycat.com/frayedcircularcattle";
+        break;
+      case 3:
+        gif = "https://media1.giphy.com/media/4bpK2k0Yru5Us/200.gif";
+        break;
+      case 4:
+        gif =
+          "https://media.tenor.com/images/c336d9b6845eac3b94ea727dd320f637/tenor.gif";
+        break;
+      case 5:
+        gif = "https://media3.giphy.com/media/cm5UUDclHfbgZYk1SW/200.gif";
+        break;
+    }
+    message.channel.send(gif);
     return;
   }
 
@@ -63,7 +82,10 @@ client.on("voiceStateUpdate", (oldMember, newMember) => {
     status: getStatus(oldUserChannel, newUserChannel),
   };
 
-  utils.log("user " + user.name + "(" + user.id + ") " + user.status, user.status === utils.userStatus.unknown);
+  utils.log(
+    "user " + user.name + "(" + user.id + ") " + user.status,
+    user.status === utils.userStatus.unknown
+  );
 
   // Meu Id
   // if (newMember.id === utils.env.ruanUserId) {
