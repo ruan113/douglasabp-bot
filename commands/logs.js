e594e30832d67d9f58f85565ea7ef877ff25eb57
Ruan Michel Adabo <ruanbento@gmail.com>
2021-01-18 20:50:55 -0300

    [UPDATE] Reset command otimization for logs

--- commands/logs.js
+++ commands/logs.js
@@ -1,4 +1,5 @@
 fs = require("fs");
+utils = require("./../utils");
 
 exports.command = {
   title: "logs",
@@ -6,16 +7,38 @@ exports.command = {
   run: (client, message, args) => {
     let logs = [];
     fs.readFile("logs.txt", "utf8", function (err, data) {
+      if (!message.member.hasPermission("ADMINISTRATOR"))
+        return message.channel.send(
+          "*You don't have permission to use this command.*"
+        );
+
       if (err) {
         console.log(err);
       }
-      logs = data.split(";");
 
-      let string = "";
-      logs.forEach(function (it) {
-        string += it + "\n";
-      });
-      message.channel.send(`Last 10 system logs:\n\n${string}`);
+      if (args[0]) {
+        switch (args[0]) {
+          case "reset":
+            resetLogs(message);
+            return;
+        }
+      }
+
+      logs = data.split(";");
+      message.channel.send(`Last 10 system logs:\n\n${logs.join("\n")}`);
     });
   },
 };
+
+function resetLogs(message) {
+  fs.writeFile("logs.txt", "", "utf8", function (err, data) {
+    if (err) {
+      utils.log(`*Error while trying to reset system logs. Details: ${err}*`);
+      return message.channel.send(`*Error while trying to reset system logs.*`);
+    } else {
+      success = `*System logs have been reseted by ${message.member.user.username}.*`;
+      utils.log(success);
+      return message.channel.send(success);
+    }
+  });
+}
