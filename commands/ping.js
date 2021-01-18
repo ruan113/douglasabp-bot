527adbb34c5bccd29b9e01238175c8bfb38ab53f
Ruan Michel Adabo <ruanbento@gmail.com>
2021-01-18 20:04:46 -0300

    [UPDATE] Ping command updated

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,13 @@ exports.command = {
   title: "ping",
   description: "Just a command to test if Douglas is listening.",
   run: (client, message, args) => {
-    message.channel.send("Estou aqui!", {
-      tts: false,
-    });
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
+    let ping = Date.now() - message.createdTimestamp;
+    message.channel.send(`
+    Ping: ~${ping}ms\nUptime: ${days}d ${hours}h ${minutes}m ${seconds}s
+    `);
   },
 };
