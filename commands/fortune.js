cca52433632d3c0c59b6bf499c97a2a184eb339c
Ruan Michel Adabo <ruanbento@gmail.com>
2021-01-25 21:18:47 -0300

    [UPDATE] Fortune has been limited by per day

--- commands/fortune.js
+++ commands/fortune.js
@@ -1,10 +1,19 @@
+const moment = require("moment");
+const db = require("quick.db");
+
 exports.command = {
   title: "ping",
   description: "Just a command to test if Douglas is listening.",
   run: (client, message, args) => {
-    const fortunes = require("./../bot-function_fortune.json");
-    message.reply(
-      `${fortunes[Math.floor(Math.random() * fortunes.length)]}`
-    );
+    const found = db.fetch(`fortune.${message.author.id}`);
+    let diff = found ? moment().diff(moment(found), "days") : undefined;
+
+    if (diff > 0 || !found) {
+      const fortunes = require("./../bot-function_fortune.json");
+      message.reply(`${fortunes[Math.floor(Math.random() * fortunes.length)]}`);
+      db.set(`fortune.${message.author.id}`, moment())
+    } else {
+      message.reply('you can only have a fortune per day!');
+    }
   },
 };
